7ab2e052f534b55158dd7fcd55e0841c3451f4ea
martin.gullaksen@gmail.com <Cool1986>
2017-08-03 10:58:48 +0200

    Added a highway network. Not tested.

--- NetworkGenerator.py
+++ NetworkGenerator.py
@@ -4,6 +4,7 @@ class NetworkGenerator:
         self.author = 'ACN'
 
     def get_alex_net(self, pic_size):
+
         """ AlexNet.
         Applying 'Alexnet' to Oxford's 17 Category Flower Dataset classification task.
         References:
@@ -14,6 +15,7 @@ class NetworkGenerator:
             - [AlexNet Paper](http://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf)
             - [Flower Dataset (17)](http://www.robots.ox.ac.uk/~vgg/data/flowers/17/)
         """
+
         from tflearn.layers.core import input_data, dropout, fully_connected
         from tflearn.layers.conv import conv_2d, max_pool_2d
         from tflearn.layers.normalization import local_response_normalization
@@ -41,3 +43,84 @@ class NetworkGenerator:
                              loss='categorical_crossentropy',
                              learning_rate=0.001)
         return network, 'AlexNet'
+
+    def get_convnet(self, pic_size):
+
+        """ Convolutional network applied to CIFAR-10 dataset classification task.
+        References:
+            Learning Multiple Layers of Features from Tiny Images, A. Krizhevsky, 2009.
+        Links:
+            [CIFAR-10 Dataset](https://www.cs.toronto.edu/~kriz/cifar.html)
+        """
+
+        import tflearn
+        from tflearn.data_utils import shuffle, to_categorical
+        from tflearn.layers.core import input_data, dropout, fully_connected
+        from tflearn.layers.conv import conv_2d, max_pool_2d
+        from tflearn.layers.estimator import regression
+        from tflearn.data_preprocessing import ImagePreprocessing
+        from tflearn.data_augmentation import ImageAugmentation
+
+        # Real-time data preprocessing
+        img_prep = ImagePreprocessing()
+        img_prep.add_featurewise_zero_center()
+        img_prep.add_featurewise_stdnorm()
+
+        # Real-time data augmentation
+        img_aug = ImageAugmentation()
+        img_aug.add_random_flip_leftright()
+        img_aug.add_random_rotation(max_angle=25.)
+
+        # Convolutional network building
+        network = input_data(shape=[None, pic_size[0], pic_size[1], 3],
+                             data_preprocessing=img_prep,
+                             data_augmentation=img_aug)
+        network = conv_2d(network, 32, 3, activation='relu')
+        network = max_pool_2d(network, 2)
+        network = conv_2d(network, 64, 3, activation='relu')
+        network = conv_2d(network, 64, 3, activation='relu')
+        network = max_pool_2d(network, 2)
+        network = fully_connected(network, 512, activation='relu')
+        network = dropout(network, 0.5)
+        network = fully_connected(network, 10, activation='softmax')
+        network = regression(network, optimizer='adam',
+                             loss='categorical_crossentropy',
+                             learning_rate=0.001)
+
+        return network, 'ConvNet'
+
+    def get_highway(self, pic_size):
+
+        """ Convolutional Neural Network for MNIST dataset classification task.
+        References:
+            Y. LeCun, L. Bottou, Y. Bengio, and P. Haffner. "Gradient-based
+            learning applied to document recognition." Proceedings of the IEEE,
+            86(11):2278-2324, November 1998.
+        Links:
+            [MNIST Dataset] http://yann.lecun.com/exdb/mnist/
+        """
+
+        from __future__ import division, print_function, absolute_import
+
+        import tflearn
+        from tflearn.layers.core import input_data, dropout, fully_connected
+        from tflearn.layers.conv import highway_conv_2d, max_pool_2d
+        from tflearn.layers.normalization import local_response_normalization, batch_normalization
+        from tflearn.layers.estimator import regression
+
+        # Building convolutional network
+        network = input_data(shape=[None, 28, 28, 1], name='input')
+        # highway convolutions with pooling and dropout
+        for i in range(3):
+            for j in [3, 2, 1]:
+                network = highway_conv_2d(network, 16, j, activation='elu')
+            network = max_pool_2d(network, 2)
+            network = batch_normalization(network)
+
+        network = fully_connected(network, 128, activation='elu')
+        network = fully_connected(network, 256, activation='elu')
+        network = fully_connected(network, 10, activation='softmax')
+        network = regression(network, optimizer='adam', learning_rate=0.01,
+                             loss='categorical_crossentropy', name='target')
+
+        return network, 'highway'
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 
-
 from __future__ import division, print_function, absolute_import
 
 import tflearn
 from DataProcessing import PreProcessor
 from NetworkGenerator import NetworkGenerator
-# import tflearn.datasets.oxflower17 as oxflower17
+from tflearn.data_utils import shuffle
 
 mode = 'TRAIN'  # Set to 'TRAIN' or 'TEST'
 
 dataset = PreProcessor()
-# dataset.video_to_images()
-# dataset.augment_data()
+# dataset.video_to_images() # make raw pictures
+# dataset.augment_data() # augment the manually cleaned raw pictures
 X, Y = dataset.load_data('./processed_data')
+X, Y = shuffle(X, Y)
+
 n_labels = Y.shape[1]
 size_pics = dataset.size_pics
 
 # Get Architecture
 net_gen = NetworkGenerator()
-network, net_name = net_gen.get_alex_net(size_pics)
+network, net_name = net_gen.get_convnet(pic_size=size_pics)
+# network, net_name = net_gen.get_alex_net(pic_size=size_pics)
+# network, net_name = net_gen.get_highway(pic_size=size_pics)
 
 # Training
 model = tflearn.DNN(network, checkpoint_path='model_' + net_name,
